a17c7e1fb00ecf2b9957f7ba7addea30c7814f31
Denireas <samsonov311@yandex.ru>
2021-03-05 21:52:30 +0400

    MVC Web App

---
+++ src/main/java/web/webMVC/config/MyMVCInitializer.java
@@ -0,0 +1,20 @@
+package web.webMVC.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyMVCInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
